0d3cedd3791985138a56dd7ed93bc70daae78d71
Oleksandr <63729507+Sentezy@users.noreply.github.com>
2020-07-15 01:25:14 +0300

    Hw_2

--- Hw_2.py
+++ Hw_2.py
@@ -27,8 +27,8 @@ def reversed_list(input_list):
     [10, 11, 2, 3, 5, 8, 23, 11, 2, 5, 76, 43, 2, 32, 76, 3, 10, 0, 1]
     вывести исходный массив в обратном порядке
     """
-    reversed = input_list[::-1]
-
+    reversed = input_list[::-1]   # or input_list.reverse()
+                                  # return input_list
     return reversed
 
 
