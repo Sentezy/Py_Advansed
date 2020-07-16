25e31f1eeeb06c7d361303e4fab5b4b43b6f2cd4
Sentezy <sentezyq@gmail.com>
2020-07-16 19:26:56 +0300

    Hw_4

---
+++ Hw_4.py
@@ -0,0 +1,26 @@
+# 1. Напишите функцию, которая возвращает список файлов из директории.
+#
+# 2. Напишите декоратор для этой функции, который распечатает все файлы с
+#
+# раcширением .log из найденных
+
+import os
+
+
+def file_reader(func):
+    def wrapper(*args):
+
+        for file in func(*args):
+            if file.endswith('.log'):
+                with open(f"{args[0]}{file}", "r", encoding="utf-8") as data:
+                    print(data.read())
+
+        return f"Files in path: {func(*args)}"  # <- Если нужен список файлов,
+                                                # потому что None бесит в конце выполнения функции
+    return wrapper
+
+
+@file_reader
+def file_finder(path):
+    for path, dirs, files in os.walk(path):
+        return files
