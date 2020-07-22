a866a19730ca24708318293e69179b566198757f
Sentezy <sentezyq@gmail.com>
2020-07-22 15:11:29 +0300

    Hw_6

---
+++ Hw_6.py
@@ -0,0 +1,65 @@
+# '''Создайте класс который будет хранить параметры для
+# подключения к физическому юниту (например сервер). В своем
+# списке атрибутов он должен иметь минимальный набор
+# (unit_name, mac_address, ip_address, login, password).
+# Вы должны описать каждый из этих атрибутов в виде гетеров и
+# сеттеров (@property). У вас должна быть возможность
+# получения и переопределения этих атрибутов в классе.'''
+
+
+class ConnHandler:
+    __slots__ = ['_unit_name',
+                 '_mac_address',
+                 '_ip_address',
+                 '_login',
+                 '_password']
+
+    def __init__(self, unit_name='', mac_address='', ip_address='', login='', password=''):
+        self._unit_name = unit_name
+        self._mac_address = mac_address
+        self._ip_address = ip_address
+        self._login = login
+        self._password = password
+
+    @property
+    def unit_name(self):
+        return self._unit_name
+
+    @unit_name.setter
+    def unit_name(self, unit_name):
+        self._unit_name = unit_name
+
+    @property
+    def mac_address(self):
+        return self._mac_address
+
+    @mac_address.setter
+    def mac_address(self, mac_address):
+        self._mac_address = mac_address
+
+    @property
+    def ip_address(self):
+        return self._ip_address
+
+    @ip_address.setter
+    def ip_address(self, ip_address):
+        self._ip_address = ip_address
+
+    @property
+    def login(self):
+        return self._login
+
+    @login.setter
+    def login(self, login):
+        self._login = login
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, password):
+        self._password = password
+
+
+
